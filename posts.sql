42389bb914c05fd01b6cc9cb64bfe67d667c00e3
Sebastian Campos <pseba@outlook.com>
2020-09-28 22:13:49 -0300

    2do commit

--- posts.sql
+++ posts.sql
@@ -1,3 +1,4 @@
+--PARTE 1
 --1
 CREATE DATABASE posts;
 --2
@@ -7,4 +8,20 @@ nombre VARCHAR(50),
 fecha DATE,
 contenido VARCHAR(250),
 descripcion VARCHAR(250));
+--3
+INSERT INTO post (nombre, fecha, contenido, descripcion) 
+VALUES ('Pamela', NOW(), 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+INSERT INTO post (nombre, fecha, contenido, descripcion) 
+VALUES ('Pamela', NOW(), 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+INSERT INTO post (nombre, fecha, contenido, descripcion) 
+VALUES ('Carlos', NOW(), 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+--4
+ALTER TABLE post
+ADD COLUMN titulo VARCHAR(50)
+DEFAULT NULL;
+--5
+INSERT INTO post (titulo) 
+VALUES ('titulo1') WHERE id = 1;
+--6
+
 
